3ce62a863749d847062bd724ce7c865db8bdfcc7
Louvani <paula.louvani@gmail.com>
2021-08-13 18:04:05 -0500

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-file {'holberton':
-  path    => /tmp/holberton
-  mode    => '0744'
-  owner   => 'www-data'
-  group   => 'www-data'
-  content => 'I love Puppet'
-  ensure  => file,
+# Task0
+file { '/tmp/holberton':
+  ensure  =>'file',
+  mode    =>'0744',
+  owner   =>'www-data',
+  group   =>'www-data',
+  content =>'I love Puppet',
 }
