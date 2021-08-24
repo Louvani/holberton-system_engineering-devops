d01f55f90105fb02884c3a5f66ea69dd4bf92526
Louvani <paula.louvani@gmail.com>
2021-08-24 11:49:33 -0500

    comming back to my coded

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,14 +6,14 @@ package { 'nginx':
   ensure  => installed,
   require => Exec['apt-get update'],
 }
-file_line { 'update default':
+file_line { 'first':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'rewrite ^/redirect_me https://ciudadseva.com/texto/extasis/ permanent;',
   require => Package['nginx'],
 }
-file_line { 'update default 2':
+file_line { 'second':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
