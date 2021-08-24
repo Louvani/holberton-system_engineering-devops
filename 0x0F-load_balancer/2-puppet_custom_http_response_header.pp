97e652a3d78eea7174efd1aaabc66938a94a89c8
Louvani <paula.louvani@gmail.com>
2021-08-24 11:27:10 -0500

    Puppet lint in task2 x2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -34,13 +34,13 @@ file_line { 'update default 2':
   require => Package['nginx'],
 }
 file_line { 'update default 3':
-    ensure  => 'present'
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default_server;',
-    line    => 'add_header X-Served-By ${HOSTNAME};',
-    require => Package['nginx'],
+  ensure  => 'present'
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By ${HOSTNAME};',
+  require => Package['nginx'],
 }
-service {
-    ensure  => running,
-    require => Package['nginx'],
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
