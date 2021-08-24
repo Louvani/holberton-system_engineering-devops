753d509e8a407eba92767020761adfcff6efe58c
Louvani <paula.louvani@gmail.com>
2021-08-24 11:25:41 -0500

    Puppet lint in task2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,44 +1,46 @@
 # 2. Add a custom HTTP header with Puppet
 exec { 'Update'
-	command =>'apt-get update',
-	path    =>'/usr/bin/',
+  command =>'apt-get update',
+  path    =>'/usr/bin/',
 }
 package { 'nginx':
-	ensure  => installed,
-	require => exec['Update'],
+  ensure  => installed,
+  require => Exec['Update'],
 }
 file { 'index.html':
-	path    => '/var/www/html/index.html',
-	ensure  => file,
-	content => "Holberton School",
-	require => package['nginx'],
+  path    => '/var/www/html/index.html',
+  ensure  => file,
+  content => "Holberton School",
+  require => Package['nginx'],
 }
 file_line { 'update default':
-	path    => '/etc/nginx/sites-available/default',
-	line    => 'location /redirect_me {\n     rewrite ^/redirect_me(.*)$ https://ciudadseva.com/texto/extasis/ permanent; \n}',
-	after   => 'server_name _;',
-	require => package['nginx'],
+  path    => '/etc/nginx/sites-available/default',
+  ensure  => 'present'
+  line    => 'rewrite ^/redirect_me(.*)$ https://ciudadseva.com/texto/extasis/ permanent; \n}',
+  after   => 'listen 80 default_server;',
+  require => Package['nginx'],
 }
 file { 'error_404.html':
-	path    => '/var/www/html/error_404.html',
-	line    => 'Ceci n'est pas une page',
-	ensure  => file,
-	content => 'Ceci n'est pas une page',
-	require => package['nginx'],
+  path    => '/var/www/html/error_404.html',
+  ensure  => file,
+  content => "Ceci n'est pas une page",
+  require => Package['nginx'],
 }
 file_line { 'update default 2':
-	path    => '/etc/nginx/sites-available/default',
-	line    => 'error_page 404 /error_404.html;',
-	after   => 'listen 80 default_server',
-	require => package['nginx'],
+  path    => '/etc/nginx/sites-available/default',
+  ensure  => 'present'
+  after   => 'listen 80 default_server;',
+  line    => 'error_page 404 /error_404.html;',
+  require => Package['nginx'],
 }
 file_line { 'update default 3':
-	path    => '/etc/nginx/sites-available/default',
-	line    => 'add_header X-Served-By ${HOSTNAME};',
-	after   => 'listen 80 default_server',
-	require => package['nginx'],
+    ensure  => 'present'
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'listen 80 default_server;',
+    line    => 'add_header X-Served-By ${HOSTNAME};',
+    require => Package['nginx'],
 }
 service {
-	ensure  => running,
-	require => package['nginx'],
+    ensure  => running,
+    require => Package['nginx'],
 }
