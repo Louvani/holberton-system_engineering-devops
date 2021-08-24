7753609cf1d5524629fcd027f160508650b13123
Louvani <paula.louvani@gmail.com>
2021-08-24 11:37:14 -0500

    more puppet lint in task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,6 @@
 # 2. Add a custom HTTP header with Puppet
 exec { 'Update'
-  command =>'apt-get update',
-  path    =>'/usr/bin/',
+  command =>'/usr/bin/apt-get update',
 }
 package { 'nginx':
   ensure  => installed,
@@ -22,7 +21,7 @@ file_line { 'update default 3':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By ${HOSTNAME};',
+  line    => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
 service { 'nginx':
