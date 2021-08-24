d3a49b3034c86b867ce5ed6aa6ccb9ba395afc57
Louvani <paula.louvani@gmail.com>
2021-08-24 11:34:38 -0500

    more puppet lint in task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,14 +12,14 @@ file { '/var/www/html/index.html':
   require => Package['nginx'],
 }
 file_line { 'update default':
+  ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
-  ensure  => 'present'
-  line    => 'rewrite ^/redirect_me(.*)$ https://ciudadseva.com/texto/extasis/ permanent;}',
   after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://ciudadseva.com/texto/extasis/ permanent;',
   require => Package['nginx'],
 }
 file_line { 'update default 3':
-  ensure  => 'present'
+  ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'add_header X-Served-By ${HOSTNAME};',
