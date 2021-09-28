19f5241992089a63c05b2a1c42676163a092f9ac
Louvani <paula.louvani@gmail.com>
2021-09-27 19:29:23 -0500

    Add else condition to print the titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,12 @@ def top_ten(subreddit):
     '''prints the titles of the first 10 hot posts
     listed for a given subreddit.'''
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    # values = {'limit': 10}
+
     response = requests.get(url)
 
     if response.status_code != 200:
         print('None')
-    req = response.json()
-    for item in req['data']['children']:
-        print(item['data']['title'])
+    else:
+        req = response.json()
+        for item in req['data']['children']:
+            print(item['data']['title'])
