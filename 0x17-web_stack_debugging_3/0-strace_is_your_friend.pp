ac37e66954d7600ed965371e0401e183245c1676
Louvani <paula.louvani@gmail.com>
2021-09-30 11:47:01 -0500

    Task1 exec way

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,5 @@
 # debug apache2
-exec { 'restart':
-  command => 'source /etc/apache2/envvars; apache2 -V; service apache2 restart',
-}
-
-file_line { 'change name':
-  ensure  => 'present',
-  path    => '/var/www/html/wp-settings.php',
-  replace => true,
-  match   => "require_once(ABSPATH . WPINC . '/class-wp-locale.phpp'",
-  line    => "require_once(ABSPATH . WPINC . '/class-wp-locale.php'",
+exec {
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart'
+  provider => 'shell',
 }
